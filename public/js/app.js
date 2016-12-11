608b3cc096fa87e48b1ca218adf3e1678863c836
Wasif Ateeq <wasifateeq007@hotmail.com>
2016-12-11 05:26:04 -0800

    adding ui for front end and handling messages between clients

--- public/js/app.js
+++ public/js/app.js
@@ -9,5 +9,32 @@ console.log("User connected too");
 socket.on("message",function(message)
 	{
 console.log("Message recieved  ");
+//Consoling message as recieved
 console.log(message.text);
 	});
+
+//Handling messages from submit button sended
+//# for id
+var $form=jQuery('#message-form');
+
+//for managing messages from other clients
+//on presssing submit button
+$form.on("submit",function(event){
+
+    //Preventing default handling of form as we want to handle it by ourself
+	event.preventDefault();
+	
+	//Getting message from form
+var message_send=$form.find("input[name]");
+	
+	//Sending message to other client
+	socket.emit("message",{
+	
+		//Sending message
+		text:message_send.val()
+	
+	});
+
+	//Making field null
+	message_send.val('');
+})
