fb7a7e9d29763305ccdd41b4bcc5e0b9d748abac
Wasif Ateeq <wasifateeq007@hotmail.com>
2016-12-11 04:46:35 -0800

    Added socket for front end and back end

--- server.js
+++ server.js
@@ -8,11 +8,18 @@ var app=express();
 //using express as a boiler plate
 //whatever express listen to http too
 var http=require('http').Server(app);
+//Requiring socket to use it
+var io=require('socket.io')(http);
 
 //for front end specifing folder
 app.use(express.static(__dirname+"/public"));
 
-
+//When user load page localhost:3000 it sends connection message and below function gets call
+//Only if socket is installed for front end too
+io.on("connection",function(){
+	console.log("Client connecting to server");
+})
+//Local host starting
 http.listen(PORT,function(){
 console.log("Listening at PORT "+PORT);
 });
